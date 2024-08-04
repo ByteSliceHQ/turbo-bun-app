377c1d01e541ba480c360326a979321aa1171829
CJ Brewer <brewercalvinj@gmail.com>
2024-08-04 12:28:54 -0600

    chore: add debug console log

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+console.log('[DEBUG] The TEST_ENV environment variable is:', process.env.TEST_ENV)
+
 app.get('/', async (c) => {
   const example = {
     hello: process.env.TEST_ENV
